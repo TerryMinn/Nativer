a7b3099191da571db1f0f60f917da5137a48d185
TerryMinn <shinnthantmindev.mm@gmail.com>
2025-01-08 23:59:14 +0700

    feat: image picker

--- app/(home)/account.tsx
+++ app/(home)/account.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "@/components/container";
 import { VStack } from "@/components/ui/vstack";
 import { Box } from "@/components/ui/box";
@@ -13,13 +13,33 @@ import {
 import { Center } from "@/components/ui/center";
 import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
+import { Pressable } from "react-native";
+import { Upload } from "lucide-react-native";
+import * as ImagePicker from "expo-image-picker";
 
 type ProfileProps = {};
 
 const Profile = (props: ProfileProps) => {
   const { logout, session } = useAuthStore();
+  const [image, setImage] = useState<string | null>(null);
 
   const { profile } = session;
+
+  const handlePickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "videos"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      console.log(result);
+    }
+  };
 
   return (
     <Container>
@@ -36,7 +56,14 @@ const Profile = (props: ProfileProps) => {
                     : require("@/assets/images/user_placeholder.jpg")
                 }
               />
-              <AvatarBadge />
+              <Pressable
+                onPress={handlePickImage}
+                className="absolute bottom-0 right-1"
+              >
+                <Box className="bg-white p-1 rounded-full border border-gray-400">
+                  <Upload color={"black"} size={12} />
+                </Box>
+              </Pressable>
             </Avatar>
             <Center>
               <Heading className="font-heading text-3xl">
@@ -46,13 +73,12 @@ const Profile = (props: ProfileProps) => {
             </Center>
           </Center>
         </VStack>
-
         {/* Sign Out */}
         <Box>
           <Button
             onPress={logout}
             variant="solid"
-            className="bg-red-500 rounded-full h-14"
+            className="bg-black rounded-full h-14"
           >
             <ButtonText className="text-white text-lg">Sign Out</ButtonText>
           </Button>
